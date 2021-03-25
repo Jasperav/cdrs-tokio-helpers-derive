364c94af2423cd0691804d9fb31ee537f2b7351e
Jasper Visser <jasper.visser@keylane.com>
2021-03-25 16:32:04 +0100

    Updated macro's for cdrs_orm

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,10 +1,3 @@
-#[macro_use]
-extern crate cdrs;
-#[macro_use]
-extern crate cdrs_tokio_helpers_derive;
-extern crate time;
-
-use time::Timespec;
 use std::collections::HashMap;
 use cdrs_tokio::types::AsRustType;
 use cdrs_tokio::types::value::{Bytes, Value};
@@ -24,7 +17,7 @@ struct Udt {
     pub vec: Vec<Vec<i32>>,
     pub map: HashMap<i64, N>,
     pub opt: Option<HashMap<i64, N>>,
-    pub my_timestamp: Option<Timespec>,
+    pub uuid: uuid::Uuid
 }
 
 // #[derive(Debug, IntoCdrsValue, TryFromRow, TryFromUdt)]
@@ -46,50 +39,11 @@ fn main() {
         vec: vec![vec![1, 2]],
         map: HashMap::new(),
         opt: Some(HashMap::new()),
-        my_timestamp: None,
+        uuid: Default::default()
     };
+
     let val: cdrs_tokio::types::value::Value = udt.clone().into();
     let values = query_values!(udt.clone());
     println!("as value {:?}", val);
     println!("among values {:?}", values);
-}
-
-#[cfg(test)]
-mod test {
-    use cdrs_tokio::query::QueryValues;
-    use cdrs_tokio::types::prelude::Value;
-
-    #[derive(DBMirror)]
-    #[allow(dead_code)]
-    struct SomeStruct {
-        pk: i32,
-        name: String,
-    }
-
-    #[test]
-    fn test_insert_query() {
-        assert_eq!("insert into SomeStruct(pk, name) values (?, ?)", SomeStruct::insert_query())
-    }
-
-    #[test]
-    fn test_into_query_values() {
-        let pk = 1;
-        let name = "some name".to_string();
-        let query_values: QueryValues = SomeStruct {
-            pk,
-            name: name.clone()
-        }.into_query_values();
-
-        if let QueryValues::NamedValues(nv) = query_values {
-            assert_eq!(2, nv.len());
-
-            let pk_val: Value = pk.into();
-            assert_eq!(&pk_val, nv.get("pk").unwrap());
-
-            let name_val: Value = name.into();
-            assert_eq!(&name_val, nv.get("name").unwrap());
-        } else {
-            panic!("Expected named values");
-        }
-    }
 }
--- src/try_from_udt.rs
+++ src/try_from_udt.rs
@@ -6,8 +6,11 @@ pub fn impl_try_from_udt(ast: &syn::DeriveInput) -> quote::Tokens {
   let name = &ast.ident;
   let fields = get_struct_fields(ast);
   quote! {
-      impl TryFromUdt for #name {
+      impl cdrs_tokio::frame::TryFromUdt for #name {
         fn try_from_udt(cdrs: cdrs_tokio::types::udt::Udt) -> cdrs_tokio::Result<Self> {
+        use cdrs_tokio::frame::TryFromUdt;
+        use cdrs_tokio::types::from_cdrs::FromCdrsByName;
+
           Ok(#name {
             #(#fields),*
           })
